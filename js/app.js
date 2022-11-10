eb592c30304adbf0da69ad800a20ad067e708187
danieldrako <ls.ortizdaniel@gmail.com>
2022-11-09 23:34:10 -0600

    after make the budget class

--- js/app.js
+++ js/app.js
@@ -10,21 +10,40 @@ function eventListeners(){
     document.addEventListener('DOMContentLoaded',preguntarPresupuesto);
 }
 
+
 // Clases
+class Presupuesto {
+    constructor(presupuesto){
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
+
+class UI {
+
+}
 
 
+//Instanciar
+const ui = new UI();
+let presupuesto;
 
 // Funciones 
-
 
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('¿Cual es tu presupuesto?')
 
-    console.log(Number(presupuestoUsuario));
+    /* console.log(Number(presupuestoUsuario)); */
 
     if(presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <=0 ) {
         window.location.reload();
     }
+
+    // Presupuesto valido
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    console.log(presupuesto)
+
 }
 
 
